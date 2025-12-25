c227a94cdd00fa3d1a7ac45e82850ecc6ae4cea2
Waldo Wautelet <Waldo.Wautelet@ae.be>
2025-12-25 14:45:42 +0100

    fix: color scheme like rider

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,9 +22,6 @@ require("lazy").setup({
     -- import your plugin
     { import = "plugin" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
