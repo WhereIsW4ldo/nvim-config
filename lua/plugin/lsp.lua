6d9e445c956f579bdc6e90ed0905b1cca7806b4d
waldo wautelet <waldowautelet4@gmail.com>
2025-12-25 22:05:55 +0100

    feat: configure roslyn for dotnet development

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -80,4 +80,12 @@ return {
       })
     end
   },
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+        -- your configuration comes here; leave empty for default settings
+    },
+  }
 }
