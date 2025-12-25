2b4ce7759aa6136ba2c3c4e0a5cae32bb91e7475
waldo wautelet <waldowautelet4@gmail.com>
2025-12-25 16:27:20 +0100

    feat: addition for LSP support for some basic language

---
+++ lua/plugin/lsp.lua
@@ -0,0 +1,39 @@
+return { 
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = {
+        "arduino_language_server",
+        "azure_pipelines_ls",
+        "bashls",
+        "cssls",
+        "docker_compose_language_service",
+        "docker_language_server",
+        "eslint",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "rust_analyzer",
+        "svelte",
+        "ts_ls"
+      }
+    },
+  },
+  {
+   'kosayoda/nvim-lightbulb'
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+    }
+  }
+}
